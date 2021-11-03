bb719321ed53798dfe5ca0d9927345aa4ac7f76d
monk1short <89112674+monk1short@users.noreply.github.com>
2021-11-03 21:34:38 +0800

    進度到門動畫做好

--- Unity_GoogleVR/Assets/Scripts/VRInteractionObject.cs
+++ Unity_GoogleVR/Assets/Scripts/VRInteractionObject.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;    //引用 事件 命名空間
 
 /// <summary>
 /// VR 互動物件
@@ -7,14 +8,26 @@ using UnityEngine;
 /// 3. VR 注視點看到此物件並點選互動按鈕 Click
 /// </summary>
 
-public class VR : MonoBehaviour
+public class VRInteractionObject : MonoBehaviour
 {
+    /// <summary>
+    /// Unity 事件用法
+    /// 1. 引用 Events 命名空間
+    /// 2. 定義 Unity Event 類型欄位並設公開
+    /// 3. 想要執行此事件內容的地方呼叫 事件.Invoke();
+    /// 4. 屬性面板設定事件內容
+    /// </summary>
+    [Header("事件:看到、離開與點擊"), Space(10)]
+    public UnityEvent onEnter;
+    public UnityEvent onExit;
+    public UnityEvent onClick;
+
     /// <summary>
     /// VR 注視點看到此物件 Enter
     /// </summary>
     public void OnPoninterEnter()
     {
-        print("注視點看到");
+        onEnter.Invoke();
     }
 
     /// <summary>
@@ -22,7 +35,7 @@ public class VR : MonoBehaviour
     /// </summary>
     public void OnPoninterExit()
     {
-        print("注視點離開");
+        onExit.Invoke();
     }
 
     /// <summary>
@@ -30,6 +43,6 @@ public class VR : MonoBehaviour
     /// </summary>
     public void OnPoninterClick()
     {
-        print("注視點點擊");
+        onClick.Invoke();
     }
 }
